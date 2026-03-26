54c7b7c37f8c11beaf06866af4013c7dc3ee21f3
Nguyen Dinh Ngoc Can <nguyendinhngoccan@gmail.com>
2026-03-27 01:16:18 +0700

    fix form congra

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Thay thế bằng URL và ANON KEY thật của bạn trong phần Project Settings -> API của Supabase
-const supabaseUrl = 'https://bvlsypbxiuqufpewyuek.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2bHN5cGJ4aXVxdWZwZXd5dWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NDY0MTQsImV4cCI6MjA5MDEyMjQxNH0.j2ZDfYqA_ewXQiPsltX4ODlp2fxuQk_Dh_FIiEU0joY'; 
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
